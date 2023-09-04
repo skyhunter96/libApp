6aeaf9dab0ad8869bb59088a1e1c5e222e81f449
skyhunter96 <misteryx96@yahoo.com>
2023-09-04 20:06:00 +0200

    LibApp.WebApp - BookViewModel Nullable Fix

--- LibApp.WebApp/ViewModels/BookViewModel.cs
+++ LibApp.WebApp/ViewModels/BookViewModel.cs
@@ -5,11 +5,10 @@ namespace LibApp.WebApp.ViewModels
 {
     public class BookViewModel
     {
-        //TODO: String nullable maybe not needed?
         public int Id { get; set; }
         public string Title { get; set; }
-        public string? Description { get; set; }
-        public string? Isbn { get; set; }
+        public string Description { get; set; }
+        public string Isbn { get; set; }
         public string Edition { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0}", ApplyFormatInEditMode = true)]
@@ -18,11 +17,11 @@ namespace LibApp.WebApp.ViewModels
         public int? CategoryId { get; set; }
         public int? DepartmentId { get; set; }
         public int? LanguageId { get; set; }
-        public string? Publisher { get; set; }
-        public string? Category { get; set; }
-        public string? Department { get; set; }
-        public string? Language { get; set; }
-        public ICollection<int>? AuthorIds { get; set; }
+        public string Publisher { get; set; }
+        public string Category { get; set; }
+        public string Department { get; set; }
+        public string Language { get; set; }
+        public ICollection<int> AuthorIds { get; set; }
         public decimal? Cost { get; set; }
         [Display(Name = "Available")]
         public bool IsAvailable { get; set; }
@@ -32,18 +31,18 @@ namespace LibApp.WebApp.ViewModels
         [Display(Name = "Reserved Quantity")]
         public int ReservedQuantity { get; set; }
 
-        public SelectList? Publishers { get; set; }
-        public SelectList? Categories { get; set; }
-        public SelectList? Departments { get; set; }
-        public SelectList? Languages { get; set; }
-        public IEnumerable<(int AuthorId, string AuthorName)>? Authors { get; set; }
-        [Display(Name = "CreatedDT")]
+        public SelectList Publishers { get; set; }
+        public SelectList Categories { get; set; }
+        public SelectList Departments { get; set; }
+        public SelectList Languages { get; set; }
+        public IEnumerable<(int AuthorId, string AuthorName)> Authors { get; set; }
+        [Display(Name = "Created")]
         public DateTime CreatedDateTime { get; set; }
-        [Display(Name = "ModifiedDT")]
+        [Display(Name = "Modified")]
         public DateTime ModifiedDateTime { get; set; }
         [Display(Name = "CreatedBy")]
-        public string? CreatedByUser { get; set; }
+        public string CreatedByUser { get; set; }
         [Display(Name = "ModifiedBy")]
-        public string? ModifiedByUser { get; set; }
+        public string ModifiedByUser { get; set; }
     }
 }
