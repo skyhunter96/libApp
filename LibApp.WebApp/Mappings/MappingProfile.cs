98d458d7c8247fc1018fcde2ccff48ca5a0614ab
skyhunter96 <misteryx96@yahoo.com>
2023-10-11 01:16:03 +0200

    LibApp.WebApp - Book MappingProfile

---
+++ LibApp.WebApp/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Domain.Models;
+using LibApp.WebApp.ViewModels;
+
+namespace LibApp.WebApp.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookViewModel>()
+                .ForMember(dest => dest.Authors, opt => opt.MapFrom(src => MapAuthors(src.Authors)))
+                .ForMember(dest => dest.Publisher, opt => opt.MapFrom(src => src.Publisher.Name))
+                .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category.Name))
+                .ForMember(dest => dest.Department, opt => opt.MapFrom(src => src.Department.Name))
+                .ForMember(dest => dest.Language, opt => opt.MapFrom(src => src.Language.Name));
+        }
+
+        private IEnumerable<(int AuthorId, string AuthorName)> MapAuthors(IEnumerable<Author> authors)
+        {
+            return authors.Select(author => (AuthorId: author.Id, AuthorName: author.Name));
+        }
+    }
+}
