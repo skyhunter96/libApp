b575fd2f4d5ce1740f8e33ac099b9a56c4ce7bca
skyhunter96 <misteryx96@yahoo.com>
2023-12-01 16:36:47 +0100

    LibApp.WebApp - HomeController Removed Dashboard Page

--- LibApp.WebApp/Controllers/HomeController.cs
+++ LibApp.WebApp/Controllers/HomeController.cs
@@ -23,11 +23,5 @@ namespace LibApp.WebApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        public IActionResult Dashboard()
-        {
-            //TODO: Dashboard() view
-            throw new NotImplementedException();
-        }
     }
 }
