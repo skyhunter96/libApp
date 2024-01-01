0feb7b6c98e7aaca862a1e702013544778a085e9
skyhunter96 <misteryx96@yahoo.com>
2024-01-01 21:12:32 +0100

    LibApp.EfDataAccess - Role Deenumazation

--- LibApp.Domain/Models/Role.cs
+++ LibApp.Domain/Models/Role.cs
@@ -1,6 +1,14 @@
-﻿namespace Domain.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Models
 {
-    public enum Role
+    public class Role : IdentityRole<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public User User { get; set; }
+    }
+    public enum RoleEnum
     {
         Admin = 1,
         Librarian = 2,
