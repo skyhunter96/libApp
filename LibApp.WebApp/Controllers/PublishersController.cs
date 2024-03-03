837581a4a1d16c5c527d7a703bf54f41703cbb4d
skyhunter96 <misteryx96@yahoo.com>
2024-03-03 23:01:50 +0100

    LibApp.WebApp - Publishers Initial Scaffold CRUD

---
+++ LibApp.WebApp/Controllers/PublishersController.cs
@@ -0,0 +1,166 @@
+﻿using Domain.Models;
+using EfDataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibApp.WebApp.Controllers
+{
+    public class PublishersController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public PublishersController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Publishers
+        public async Task<IActionResult> Index()
+        {
+            var libraryContext = _context.Publishers.Include(p => p.CreatedByUser).Include(p => p.ModifiedByUser);
+            return View(await libraryContext.ToListAsync());
+        }
+
+        // GET: Publishers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publishers
+                .Include(p => p.CreatedByUser)
+                .Include(p => p.ModifiedByUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(publisher);
+        }
+
+        // GET: Publishers/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatedByUserId"] = new SelectList(_context.Users, "Id", "City");
+            ViewData["ModifiedByUserId"] = new SelectList(_context.Users, "Id", "City");
+            return View();
+        }
+
+        // POST: Publishers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description,Id,CreatedDateTime,ModifiedDateTime,CreatedByUserId,ModifiedByUserId")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(publisher);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedByUserId"] = new SelectList(_context.Users, "Id", "City", publisher.CreatedByUserId);
+            ViewData["ModifiedByUserId"] = new SelectList(_context.Users, "Id", "City", publisher.ModifiedByUserId);
+            return View(publisher);
+        }
+
+        // GET: Publishers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatedByUserId"] = new SelectList(_context.Users, "Id", "City", publisher.CreatedByUserId);
+            ViewData["ModifiedByUserId"] = new SelectList(_context.Users, "Id", "City", publisher.ModifiedByUserId);
+            return View(publisher);
+        }
+
+        // POST: Publishers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Description,Id,CreatedDateTime,ModifiedDateTime,CreatedByUserId,ModifiedByUserId")] Publisher publisher)
+        {
+            if (id != publisher.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(publisher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PublisherExists(publisher.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedByUserId"] = new SelectList(_context.Users, "Id", "City", publisher.CreatedByUserId);
+            ViewData["ModifiedByUserId"] = new SelectList(_context.Users, "Id", "City", publisher.ModifiedByUserId);
+            return View(publisher);
+        }
+
+        // GET: Publishers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publishers
+                .Include(p => p.CreatedByUser)
+                .Include(p => p.ModifiedByUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(publisher);
+        }
+
+        // POST: Publishers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var publisher = await _context.Publishers.FindAsync(id);
+            if (publisher != null)
+            {
+                _context.Publishers.Remove(publisher);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return _context.Publishers.Any(e => e.Id == id);
+        }
+    }
+}
