189cc6de94cde680c9c7cde798d311e641964f40
skyhunter96 <misteryx96@yahoo.com>
2024-03-04 01:56:50 +0100

    LibApp.WebApp - Languages Initial Scaffold CRUD

---
+++ LibApp.WebApp/Controllers/LanguagesController.cs
@@ -0,0 +1,152 @@
+﻿using Domain.Models;
+using EfDataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibApp.WebApp.Controllers
+{
+    public class LanguagesController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public LanguagesController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Languages
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Languages.ToListAsync());
+        }
+
+        // GET: Languages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Languages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return View(language);
+        }
+
+        // GET: Languages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Languages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Language language)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(language);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(language);
+        }
+
+        // GET: Languages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Languages.FindAsync(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+            return View(language);
+        }
+
+        // POST: Languages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Language language)
+        {
+            if (id != language.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(language);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LanguageExists(language.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(language);
+        }
+
+        // GET: Languages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Languages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return View(language);
+        }
+
+        // POST: Languages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var language = await _context.Languages.FindAsync(id);
+            if (language != null)
+            {
+                _context.Languages.Remove(language);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LanguageExists(int id)
+        {
+            return _context.Languages.Any(e => e.Id == id);
+        }
+    }
+}
